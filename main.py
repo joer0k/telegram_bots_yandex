6b6140823073a1faf2ccff651f6e0ccdad9320e5
joer0k <ryabokon.niki08@gmail.com>
2025-05-04 17:37:27 +0300

    'диалоговый бот'

--- main.py
+++ main.py
@@ -1,109 +1,50 @@
-from random import randint
-
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Application
+from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
 
 from config import BOT_TOKEN
 
-TIMER = 5
-
-start_keyboard = [['/dice', '/timer']]
-dice_keyboard = [['/6', '/2x6', '/20'], ['/back']]
-timer_keyboard = [['/30s', '/1m', '/5m'], ['/back']]
-close_keyboard = [['close']]
-
-start_markup = ReplyKeyboardMarkup(start_keyboard, one_time_keyboard=False)
-dice_markup = ReplyKeyboardMarkup(dice_keyboard, one_time_keyboard=False)
-timer_markup = ReplyKeyboardMarkup(timer_keyboard, one_time_keyboard=False)
-close_markup = ReplyKeyboardMarkup(close_keyboard, one_time_keyboard=False)
-
-
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
-
 
 async def start(update, context):
-    await update.message.reply_text('Привет! Я - бот-помощник для игр')
-    await update.message.reply_text('/dice - кинуть кубик\n/timer - засечь время', reply_markup=start_markup)
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+    return 1
 
 
-async def dice(update, context):
-    await update.message.reply_text('Выберите какой кубик кинуть: 6-игранный, 2 6-и гранных, 20-и гранный',
-                                    reply_markup=dice_markup)
+async def first_response(update, context):
+    locality = update.message.text
+    if locality == '/skip':
+        await update.message.reply_text('Какая погода у вас за окном?')
+    else:
+        await update.message.reply_text(
+            f"Какая погода в городе {locality}?")
+    return 2
 
 
-async def dice_6(update, context):
-    await update.message.reply_text(f'{randint(1, 6)}')
+async def second_response(update, context):
+    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    return ConversationHandler.END  # Константа, означающая конец диалога.
 
 
-async def dice_2x6(update, context):
-    await update.message.reply_text(f'{randint(1, 6)} {randint(1, 6)}')
-
-
-async def dice_20(update, context):
-    await update.message.reply_text(f'{randint(1, 20)}')
-
-
-async def timers(update, context):
-    await update.message.reply_text('Засечь время: 30s, 1m, 5m', reply_markup=timer_markup)
-
-
-async def set_timer(update, context, time):
-    job = context.job_queue.run_once(time, chat_id=update.message.chat_id, data=time)
-
-    context.chat_data['job'] = job
-    await update.message.reply_text(f'Засек {time} секунд', reply_markup=close_markup)
-
-
-async def timer_ran_out(update, context):
-    await context.bot.send_message(chat_id=update.message.chat_id, text='Время вышло', reply_markup=timer_markup)
-
-
-async def unset(update, context):
-    """Удаляет задачу, если пользователь передумал"""
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
-    await update.message.reply_text(text)
-
-
-async def timer_30s(update, context):
-    await set_timer(update, context, 30)
-
-
-async def timer_1m(update, context):
-    await set_timer(update, context, 60)
-
-
-async def timer_5m(update, context):
-    await set_timer(update, context, 300)
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
-    application.add_handler(CommandHandler('start', start))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            1: [MessageHandler(filters.TEXT, first_response)],
+            2: [MessageHandler(filters.TEXT, second_response)]
+        },
 
-    application.add_handler(CommandHandler('dice', dice))
+        fallbacks=[CommandHandler('stop', stop)]
+    )
 
-    application.add_handler(CommandHandler('back', start))
-
-    application.add_handler(CommandHandler('timer', timers))
-
-    application.add_handler(CommandHandler('6', dice_6))
-    application.add_handler(CommandHandler('2x6', dice_2x6))
-    application.add_handler(CommandHandler('20', dice_20))
-
-    application.add_handler(CommandHandler('30s', timer_30s))
-    application.add_handler(CommandHandler('1m', timer_1m))
-    application.add_handler(CommandHandler('5m', timer_5m))
+    application.add_handler(conv_handler)
 
     application.run_polling()
 
