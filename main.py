2ab60aecb8a24ea6ef78c6f1ca278f20d339418e
joer0k <ryabokon.niki08@gmail.com>
2025-05-06 15:30:02 +0300

    'бот литератор'

--- main.py
+++ main.py
@@ -1,70 +1,76 @@
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
+import logging
+
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 from config import BOT_TOKEN
 
-room_text = {
-    'enter': 'Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!',
-    'exit': 'Всего доброго, не забудьте забрать верхнюю одежду в гардеробе!',
-    'first': 'В данном зале представлены скульптуры греческих богов\nОтсюда вы можете перейти во второй зал или отправиться к выходу',
-    'second': 'В данном зале представлены скелеты динозавров\nОтсюда вы можете перейти в третий зал',
-    'third': 'В данном зале представлены самые первые компьютеры\nОтсюда вы можете перейти в первый или четвертый зал',
-    'fourth': 'В данном зале представлены машины военного времени\nОтсюда вы можете перейти в первый зал'
-}
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
 
-base_keyboard = [['/enter']]
-first_keyboard = [['/room_2', '/exit']]
-second_keyboard = [['/room_3']]
-third_keyboard = [['/room_1', '/room_4']]
-fourth_keyboard = [['/room_1']]
+logger = logging.getLogger(__name__)
+text = '''Звезда полей, во мгле заледенелой
+Остановившись, смотрит в полынью.
+Уж на часах двенадцать прозвенело,
+И сон окутал родину мою…
+Звезда полей! В минуты потрясений
+Я вспоминал, как тихо за холмом
+Она горит над золотом осенним,
+Она горит над зимним серебром…
+Звезда полей горит, не угасая,
+Для всех тревожных жителей земли,
+Своим лучом приветливым касаясь
+Всех городов, поднявшихся вдали.
+Но только здесь, во мгле заледенелой,
+Она восходит ярче и полней,
+И счастлив я, пока на свете белом
+Горит, горит звезда моих полей…'''.split('\n')
 
-base_markup = ReplyKeyboardMarkup(base_keyboard, one_time_keyboard=False)
-first_markup = ReplyKeyboardMarkup(first_keyboard, one_time_keyboard=False)
-second_markup = ReplyKeyboardMarkup(second_keyboard, one_time_keyboard=False)
-third_markup = ReplyKeyboardMarkup(third_keyboard, one_time_keyboard=False)
-fourth_markup = ReplyKeyboardMarkup(fourth_keyboard, one_time_keyboard=False)
+CURRENT = 10 ** 10
 
 
 async def start(update, context):
-    await update.message.reply_text(
-        "Привет! Я - бот-экскурсовод. Не хочешь прогуляться по музею?", reply_markup=base_markup)
+    global CURRENT
+    CURRENT = 1
+    await update.message.reply_text(text[0])
 
 
-async def enter(update, context):
-    await update.message.reply_text(room_text['enter'], reply_markup=fourth_markup)
+async def stop(update, context):
+    global CURRENT
+    CURRENT = 10 ** 10
+    await update.message.reply_text('Чтобы начать сначала введите /start')
 
 
-async def first(update, context):
-    await update.message.reply_text(room_text['first'], reply_markup=first_markup)
+async def suphler(update, context):
+    global CURRENT
+    await update.message.reply_text(text[CURRENT])
 
 
-async def second(update, context):
-    await update.message.reply_text(room_text['second'], reply_markup=second_markup)
-
-
-async def third(update, context):
-    await update.message.reply_text(room_text['third'], reply_markup=third_markup)
-
-
-async def fourth(update, context):
-    await update.message.reply_text(room_text['fourth'], reply_markup=fourth_markup)
-
-
-async def exit(update, context):
-    await update.message.reply_text(room_text['exit'], reply_markup=base_markup)
+async def user_response(update, context):
+    global CURRENT
+    try:
+        if update.message.text == text[CURRENT]:
+            CURRENT += 2
+            if CURRENT >= len(text):
+                await update.message.reply_text("Ура, у нас получилось!\nВведи /start, чтобы начать сначала")
+            if CURRENT <= len(text):
+                await update.message.reply_text(text[CURRENT - 1])
+        else:
+            await update.message.reply_text('Нет, не так')
+            await suphler(update, context)
+    except IndexError:
+        await update.message.reply_text('Введите /start')
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('enter', enter))
-    application.add_handler(CommandHandler('exit', exit))
 
-    application.add_handler(CommandHandler('room_1', first))
-    application.add_handler(CommandHandler('room_2', second))
-    application.add_handler(CommandHandler('room_3', third))
-    application.add_handler(CommandHandler('room_4', fourth))
+    application.add_handler(CommandHandler('stop', stop))
+    application.add_handler(CommandHandler('suphler', suphler))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, user_response))
+
     application.run_polling()
 
 
