b985310d40f5ca4cd0cd425ecb847b29dd89ba51
joer0k <ryabokon.niki08@gmail.com>
2025-05-07 14:03:27 +0300

    'бот тестирующая система'

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import random
+from json import loads
 
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
@@ -8,69 +10,70 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
-logger = logging.getLogger(__name__)
-text = '''Звезда полей, во мгле заледенелой
-Остановившись, смотрит в полынью.
-Уж на часах двенадцать прозвенело,
-И сон окутал родину мою…
-Звезда полей! В минуты потрясений
-Я вспоминал, как тихо за холмом
-Она горит над золотом осенним,
-Она горит над зимним серебром…
-Звезда полей горит, не угасая,
-Для всех тревожных жителей земли,
-Своим лучом приветливым касаясь
-Всех городов, поднявшихся вдали.
-Но только здесь, во мгле заледенелой,
-Она восходит ярче и полней,
-И счастлив я, пока на свете белом
-Горит, горит звезда моих полей…'''.split('\n')
-
-CURRENT = 10 ** 10
+QUESTIONS = {}
+IS_GAME = False
+user_answered = {}
+last = ''
 
 
 async def start(update, context):
-    global CURRENT
-    CURRENT = 1
-    await update.message.reply_text(text[0])
+    await update.message.reply_text('Не хотите ли вы пройти опрос?\n/yes - да\n/no - нет')
 
 
 async def stop(update, context):
-    global CURRENT
-    CURRENT = 10 ** 10
-    await update.message.reply_text('Чтобы начать сначала введите /start')
+    global IS_GAME
+    IS_GAME = False
+    await update.message.reply_text('Приходите, когда будете готовы')
 
 
-async def suphler(update, context):
-    global CURRENT
-    await update.message.reply_text(text[CURRENT])
+async def yes(update, context):
+    if QUESTIONS:
+        global IS_GAME, user_answered, last
+        IS_GAME = True
+        question = random.choice(list(QUESTIONS.keys()))
+        user_answered = {question: ''}
+        last = question
+        await update.message.reply_text(question)
+    else:
+        await update.message.reply_text('Сначала введите вопросы в формате json')
 
 
-async def user_response(update, context):
-    global CURRENT
-    try:
-        if update.message.text == text[CURRENT]:
-            CURRENT += 2
-            if CURRENT >= len(text):
-                await update.message.reply_text("Ура, у нас получилось!\nВведи /start, чтобы начать сначала")
-            if CURRENT <= len(text):
-                await update.message.reply_text(text[CURRENT - 1])
+async def get_questions(update, context):
+    global IS_GAME, QUESTIONS, user_answered, last
+    if not IS_GAME:
+        try:
+            for res in loads(update.message.text)['test']:
+                QUESTIONS[res['question']] = res['response']
+            await update.message.reply_text('Вопросы успешно записаны\nНачать тест - /yes')
+        except Exception as e:
+            await update.message.reply_text(f'Ошибка: {e}')
+    else:
+        user_answered[last] = update.message.text.strip()
+        if len(user_answered.keys()) == len(QUESTIONS.keys()) or len(user_answered.keys()) == 10:
+            IS_GAME = False
+            await update.message.reply_text(
+                f'Тест окончен. Правильных ответов - {len([value for key, value in user_answered.items() if value == QUESTIONS[key]])}')
         else:
-            await update.message.reply_text('Нет, не так')
-            await suphler(update, context)
-    except IndexError:
-        await update.message.reply_text('Введите /start')
+            question = random.choice(list(QUESTIONS.keys()))
+            while question in user_answered.keys():
+                question = random.choice(list(QUESTIONS.keys()))
+            user_answered[question] = ''
+            last = question
+            await update.message.reply_text(question)
+
+
+async def no(update, context):
+    await update.message.reply_text('Ничего страшного, жду вас снова')
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler('start', start))
-
     application.add_handler(CommandHandler('stop', stop))
-    application.add_handler(CommandHandler('suphler', suphler))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, user_response))
-
+    application.add_handler(CommandHandler('yes', yes))
+    application.add_handler(CommandHandler('no', no))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_questions))
     application.run_polling()
 
 
